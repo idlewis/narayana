152f5ccfecda2c0155a2d57c1b70239f1386c869
ilewis <idlewis@users.noreply.github.com>
2020-11-19 17:04:36 +0000

    Add a bit of javadoc

--- rts/lra/lra-proxy/api/src/main/java/io/narayana/lra/client/internal/proxy/nonjaxrs/LraClassFinder.java
+++ rts/lra/lra-proxy/api/src/main/java/io/narayana/lra/client/internal/proxy/nonjaxrs/LraClassFinder.java
@@ -3,8 +3,21 @@ package io.narayana.lra.client.internal.proxy.nonjaxrs;
 import java.io.IOException;
 import java.util.Set;
 
+/**
+ * An extension intended to be used with ServiceLoader to allow
+ * user to provide a list of LRA application classes with non-JAX-RS
+ * methods
+ */
 public interface LraClassFinder {
 
+    /**
+     * Should return a Set of classes that are:
+     *   - real (not abstract or interfaces)
+     *   - LRA participants
+     * Should throw an IllegalStateException if any classes are found which are invalid
+     * LRA participants, i.e. they have LRA annotations, but do not have one of either
+     * a compensate or afterLRA annotation
+     */
     Set<Class<?>> getClasses() throws IOException, ClassNotFoundException;
 
 }
